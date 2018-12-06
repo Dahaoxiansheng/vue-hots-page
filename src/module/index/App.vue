86fe035d51534dcb7101376eb38249ea30d43921
黄冠豪 <huanggh@shushangyun.com>
2018-12-06 13:11:53 +0800

    配置多页面初始化

---
+++ src/module/index/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+      我是首页
+      <button @clikc="topage(1)">点击跳到产品页面</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+    }
+  },
+  methods: {
+      topage(inx){
+          console.log(inx)
+          location.href = "../detail/detail.html";
+      }
+  }
+}
+</script>
+
+
+<style lang="scss" scoped>
+ #app{
+     height: 600px;
+     background-color: red;
+ }
+</style>
